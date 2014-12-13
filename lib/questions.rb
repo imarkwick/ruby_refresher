f4d16219b8ed68991970c6f35c22043572bd8d86
imarkwick <izzymarkwick@gmail.com>
2014-12-13 16:15:34 +0000

    first three complete

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,3 +1,11 @@
 def select_elements_starting_with_a(array)
 	array.select { |v| v if v[0] == 'a' }
 end
+
+def select_elements_starting_with_vowel(array)
+	array.select { |v| v if "aeiou".include?(v[0]) }
+end
+
+def remove_nils_from_array(array)
+	array.delete_if { |v| v == nil }
+end
